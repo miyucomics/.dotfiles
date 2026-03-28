f2f9e0ef6024f810f0e841592a55353c13292bb0
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-03-27 23:53:53 -0400

    Dotfiles sync, changed to awww

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -3,22 +3,22 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	opts = {
-		color_overrides = {
-			mocha = {
-				text = "#f4cde9",
-				subtext1 = "#debad4",
-				subtext0 = "#c8a6be",
-				overlay2 = "#b293a8",
-				overlay1 = "#9c7f92",
-				overlay0 = "#866c7d",
-				surface2 = "#705867",
-				surface1 = "#5a4551",
-				surface0 = "#44313b",
-				base = "#352939",
-				mantle = "#211924",
-				crust = "#1a1016",
-			},
-		},
+		-- color_overrides = {
+		-- 	mocha = {
+		-- 		text = "#f4cde9",
+		-- 		subtext1 = "#debad4",
+		-- 		subtext0 = "#c8a6be",
+		-- 		overlay2 = "#b293a8",
+		-- 		overlay1 = "#9c7f92",
+		-- 		overlay0 = "#866c7d",
+		-- 		surface2 = "#705867",
+		-- 		surface1 = "#5a4551",
+		-- 		surface0 = "#44313b",
+		-- 		base = "#352939",
+		-- 		mantle = "#211924",
+		-- 		crust = "#1a1016",
+		-- 	},
+		-- },
 	},
 	init = function()
 		vim.cmd.colorscheme("catppuccin")
